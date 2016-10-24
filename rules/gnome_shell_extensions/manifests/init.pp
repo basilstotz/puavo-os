e8174105d7a84279228d5b6d0ae0f9568bf0be72
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-10-24 15:28:22 +0300

    Fix configuration regarding bottompanel-extension.

--- rules/gnome_shell_extensions/manifests/init.pp
+++ rules/gnome_shell_extensions/manifests/init.pp
@@ -3,14 +3,14 @@ class gnome_shell_extensions {
 
   file {
     '/usr/share/gnome-shell/extensions/bigtouch-ux@puavo.org':
-      source  => 'puppet:///modules/gnome_shell_extensions/bigtouch-ux',
       recurse => true,
-      require => Package['gnome-shell-extensions'];
+      require => Package['gnome-shell-extensions'],
+      source  => 'puppet:///modules/gnome_shell_extensions/bigtouch-ux';
 
-    '/usr/share/gnome-shell/extensions/bottompanel@tmoer93':
-      source  => 'puppet:///modules/gnome_shell_extensions/gnome-shell-bottom-panel',
+  '/usr/share/gnome-shell/extensions/bottompanel@tmoer93':
       recurse => true,
-      require => Package['gnome-shell-extensions'];
+      require => Package['gnome-shell-extensions'],
+      source  => 'puppet:///modules/gnome_shell_extensions/bottompanel@tmoer93';
 
   }
 
