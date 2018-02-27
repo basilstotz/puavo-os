011bafa31fb80d4a52d7a88380e278ca67466b11
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-02-27 14:39:44 +0200

    Enable "bootserver_cron"-puppet-module for bootservers.

--- rules/image/manifests/bundle/bootserver.pp
+++ rules/image/manifests/bundle/bootserver.pp
@@ -1,6 +1,7 @@
 class image::bundle::bootserver {
   include ::bootserver_autopoweron
   # include ::bootserver_backup         # XXX needs work
+  include ::bootserver_cron
   include ::packages
 
   Package <| tag == tag_puavo_bootserver |>
