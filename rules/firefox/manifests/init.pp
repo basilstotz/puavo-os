29b74422f93a10acb0a2fee83c0721fb4a5e6c38
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-03-20 08:18:01 +0200

    Change Firefox configurations from "firefox-esr" --> "firefox".

--- rules/firefox/manifests/init.pp
+++ rules/firefox/manifests/init.pp
@@ -1,30 +1,33 @@
 class firefox {
   include ::packages
+  include ::puavo_pkg
 
   # Firefox configuration system is still a mess... if there really is a more
   # straightforward way, I would like to hear about it.
   file {
-    '/etc/firefox-esr/puavodesktop.js':
-      content => template('firefox/puavodesktop.js'),
-      require => Package['firefox-esr'];
+    '/etc/firefox':
+      ensure => directory;
 
-    '/etc/firefox-esr/syspref.js':
+    '/etc/firefox/puavodesktop.js':
+      content => template('firefox/puavodesktop.js');
+
+    '/etc/firefox/syspref.js':
       content => template('firefox/syspref.js'),
-      require => File['/usr/lib/firefox-esr/firefox-puavodesktop.js'];
-
-    '/etc/X11/Xsession.d/48puavo-set-apiserver-envvar':
-      content => template('firefox/48puavo-set-apiserver-envvar');
-
-    '/usr/lib/firefox-esr/firefox-puavodesktop.js':
-      content => template('firefox/firefox-puavodesktop.js'),
-      require => File['/etc/firefox-esr/puavodesktop.js'];
+      require => File['/opt/firefox/firefox-puavodesktop.js'];
 
     '/etc/puavo-external-files-actions.d/firefox':
       content => template('firefox/puavo-external-files-actions.d/firefox'),
       mode    => '0755',
       require => Package['puavo-ltsp-client'];
+
+    '/etc/X11/Xsession.d/48puavo-set-apiserver-envvar':
+      content => template('firefox/48puavo-set-apiserver-envvar');
+
+    '/opt/firefox/firefox-puavodesktop.js':
+      content => template('firefox/firefox-puavodesktop.js'),
+      require => Puavo_pkg::Install['firefox'];
   }
 
-  Package <| title == firefox-esr
-          or title == puavo-ltsp-client |>
+  Package <| title == puavo-ltsp-client |>
+  Puavo_pkg::Install <| title == firefox |>
 }
